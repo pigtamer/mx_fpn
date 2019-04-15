4aff3bb683119ceb7d319d500927ed347e5a5a2b
pigtamer <1617752166@qq.com>
2019-04-15 13:46:07 +0800

    add vgg-ssd-fpn

--- fpn/fpn.py
+++ fpn/fpn.py
@@ -21,17 +21,26 @@ class ChannelAdapt(nn.Block):
         return self.net(x)
 
 
-def fusionFMaps(lMap, sMap, neighbor_scale=2, method='upconv'):
+def getChannelAdapt(out_channels):
+    net = nn.Sequential()
+    net.add(
+        nn.Conv2D(channels=out_channels, kernel_size=(1, 1)),
+        nn.BatchNorm(in_channels=out_channels),
+        nn.Activation(activation='relu')
+    )
+    return net
+
+
+def fusionFMaps(lMap, sMap, method='upconv'):
     if method == 'upconv':
         raise Exception("NOT IMPLEMENTED YET.")
-        upconver = nd.Deconvolution(data=sMap, kernel=upconv_ksize,
-                                    num_filter=512, stride=(1, 1), weight=nd.random.normal(0, 1, (512, 512, 3, 3)))
-        upconver = nd.Activation(data=upconver, act_type='relu')
-        upconv_sMap = nd.BatchNorm(data=upconver, gamma=nd.random_gamma(alpha=9, beta=0.5, shape=(2, 2)))
-        # upconver.initialize(ctx=mx.gpu())  # how to init? should I make the params trainable?
-        # TODO: Modify this. Figure out a way to deal with size problem brought by pooling
-        upconv_sMap = nd.contrib.BilinearResize2D(data=sMap,
-                                                  height=lMap.shape[-2], width=lMap.shape[-1])
+        # upconver = nd.Deconvolution(data=sMap, kernel=upconv_ksize,
+        #                             num_filter=512, stride=(1, 1), weight=nd.random.normal(0, 1, (512, 512, 3, 3)))
+        # upconver = nd.Activation(data=upconver, act_type='relu')
+        # upconv_sMap = nd.BatchNorm(data=upconver, gamma=nd.random_gamma(alpha=9, beta=0.5, shape=(2, 2)))
+        # # upconver.initialize(ctx=mx.gpu())  # how to init? should I make the params trainable?
+        # upconv_sMap = nd.contrib.BilinearResize2D(data=sMap,
+        #                                           height=lMap.shape[-2], width=lMap.shape[-1])
     elif method == 'bilinear':
         upconv_sMap = nd.contrib.BilinearResize2D(data=sMap,
                                                   height=lMap.shape[-2], width=lMap.shape[-1])
@@ -44,6 +53,61 @@ def fusionFMaps(lMap, sMap, neighbor_scale=2, method='upconv'):
     return res
 
 
+class VGG_FPN(nn.Block):
+    def __init__(self, num_layers=3, num_classes=1, **kwargs):
+        super().__init__(**kwargs)
+        self.BaseBlk = ssd.BaseNetwork()
+        self.feature_blk_1 = nn.Sequential()
+        self.feature_blk_1.add(
+            nn.Conv2D(channels=512, kernel_size=3, padding=1, activation='relu'),
+            nn.Conv2D(channels=512, kernel_size=3, padding=1, activation='relu'),
+            nn.Conv2D(channels=1024, kernel_size=3, padding=1, activation='relu'),
+            nn.MaxPool2D(pool_size=(2, 2))
+        )
+        self.feature_blk_2 = nn.Sequential()
+        self.feature_blk_2.add(
+            nn.Conv2D(channels=512, kernel_size=3, padding=1, activation='relu'),
+            nn.Conv2D(channels=512, kernel_size=3, padding=1, activation='relu'),
+            nn.Conv2D(channels=512, kernel_size=3, padding=1, activation='relu'),
+            nn.MaxPool2D(pool_size=(2, 2))
+        )
+        self.ssd_1 = ssd.LightSSD(num_cls=1, num_ach=num_anchors)
+        self.ssd_2 = ssd.LightSSD(num_cls=1, num_ach=num_anchors)
+        self.ssd_3 = ssd.LightSSD(num_cls=1, num_ach=num_anchors)
+
+        self.chan_align_32 = getChannelAdapt(out_channels=1024)
+        self.chan_align_21 = getChannelAdapt(out_channels=256)
+
+    def forward(self, x):
+        x = self.BaseBlk(x)
+        fmap_1 = x
+        fmap_2 = self.feature_blk_1(fmap_1)
+        fmap_3 = self.feature_blk_2(fmap_2)
+
+        fusion_33 = fmap_3  # placeholder. to be deleted in the future
+        fusion_32 = fusionFMaps(fmap_2, self.chan_align_32(fusion_33),
+                                method='bilinear')
+        fusion_21 = fusionFMaps(fmap_1, self.chan_align_21(fusion_32),
+                                method='bilinear')
+
+        anchors, cls_preds, bbox_preds = [None] * 3, [None] * 3, [None] * 3
+        anchors[2], cls_preds[2], bbox_preds[2] = self.ssd_3(fusion_33)
+        anchors[1], cls_preds[1], bbox_preds[1] = self.ssd_2(fusion_32)
+        anchors[0], cls_preds[0], bbox_preds[0] = self.ssd_1(fusion_21)
+
+        print("-----------------------------------------------\n"
+              "FPN:     [top -> bottom]\n"
+              "         fusion[3]: %s;\n"
+              "         fusion[2]: %s;\n"
+              "         fusion[1]: %s;\n"
+              "         input    : %s;\n"
+              "-----------------------------------------------\n"
+              % (fusion_33.shape, fusion_32.shape, fusion_21.shape, x.shape))
+        return (nd.concat(*anchors, dim=1),
+                nd.concat(*cls_preds, dim=1),
+                nd.concat(*bbox_preds, dim=1))
+
+
 class ResNet_FPN(nn.Block):
     def __init__(self, num_layers=3, num_classes=1, **kwargs):
         super().__init__(**kwargs)
@@ -51,7 +115,8 @@ class ResNet_FPN(nn.Block):
             params={
                 "channels":
                     [[[64, 64, 256]] * 3,
-                     [[128, 128, 512]] * 4],
+                     [[128, 128, 512]] * 4],  # two residual layers.
+                # each having 3 or 4 units
                 "kernel_sizes":
                     [[[1, 3, 1]] * 3,
                      [[1, 3, 1]] * 4],
@@ -85,8 +150,8 @@ class ResNet_FPN(nn.Block):
         self.ssd_2 = ssd.LightRetina(num_cls=1, num_ach=retina_num_anchors)
         self.ssd_3 = ssd.LightRetina(num_cls=1, num_ach=retina_num_anchors)
 
-        self.chan_align_32 = ChannelAdapt(out_channels=1024)
-        self.chan_align_21 = ChannelAdapt(out_channels=512)
+        self.chan_align_32 = getChannelAdapt(out_channels=1024)
+        self.chan_align_21 = getChannelAdapt(out_channels=512)
 
     def forward(self, x):
         x = self.BaseBlk(x)
@@ -96,23 +161,23 @@ class ResNet_FPN(nn.Block):
 
         fusion_33 = fmap_3  # placeholder. to be deleted in the future
         fusion_32 = fusionFMaps(fmap_2, self.chan_align_32(fusion_33),
-                                neighbor_scale=4, method='bilinear')
+                                method='bilinear')
         fusion_21 = fusionFMaps(fmap_1, self.chan_align_21(fusion_32),
-                                neighbor_scale=4, method='bilinear')
+                                method='bilinear')
 
         anchors, cls_preds, bbox_preds = [None] * 3, [None] * 3, [None] * 3
         anchors[2], cls_preds[2], bbox_preds[2] = self.ssd_3(fusion_33)
         anchors[1], cls_preds[1], bbox_preds[1] = self.ssd_2(fusion_32)
         anchors[0], cls_preds[0], bbox_preds[0] = self.ssd_1(fusion_21)
 
-        # print("-----------------------------------------------\n"
-        #       "FPN:     [top -> bottom]\n"
-        #       "         fusion[3]: %s;\n"
-        #       "         fusion[2]: %s;\n"
-        #       "         fusion[1]: %s;\n"
-        #       "         input    : %s;\n"
-        #       "-----------------------------------------------\n"
-        #       %(fusion_33.shape, fusion_32.shape, fusion_21.shape, x.shape))
+        print("-----------------------------------------------\n"
+              "FPN:     [top -> bottom]\n"
+              "         fusion[3]: %s;\n"
+              "         fusion[2]: %s;\n"
+              "         fusion[1]: %s;\n"
+              "         input    : %s;\n"
+              "-----------------------------------------------\n"
+              % (fusion_33.shape, fusion_32.shape, fusion_21.shape, x.shape))
         return (nd.concat(*anchors, dim=1),
                 nd.concat(*cls_preds, dim=1),
                 nd.concat(*bbox_preds, dim=1))
